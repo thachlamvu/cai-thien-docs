eed7d9a734c21028743be0e1a63c074ea18faf96
thachlamvu <thachlamvu.w@gmail.com>
2022-05-16 22:41:34 +0700

    fibonacci

---
+++ java/fibonacci/Fibonacci/src/App.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n;
+        Scanner ip = new Scanner(System.in);
+
+        System.out.print("Nhap so luong N = ");
+        n = ip.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 1;
+
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            for (int i = 2; i < n; i++) {
+                f0 = f1;
+                f1 = fn;
+                fn = f0 + f1;
+            }
+        }
+        return fn;
+    }
+
+    public static int fibonacciDeQuy(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
